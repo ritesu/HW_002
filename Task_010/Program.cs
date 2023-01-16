31914f7d86c7efcda9c67a7450a2ef3ccce7130d
ritesu <ritesu@mail.ru>
2023-01-16 16:38:13 +0900

    Task_010

---
+++ Task_010/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()?? "0");
+if (a < 100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+else if (a >= 100 && a < 1000)
+{
+    int b = a % 10;
+    Console.WriteLine("Третье число равно");
+    Console.WriteLine(b);
+}
