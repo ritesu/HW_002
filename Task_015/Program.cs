603dc113feee4673bcb7f68a3929cde721d8587d
ritesu <ritesu@mail.ru>
2023-01-16 16:54:38 +0900

    Task_015

---
+++ Task_015/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели");
+int a = int.Parse(Console.ReadLine()?? "0");
+while(a > 7 || a < 1)
+{
+    Console.WriteLine("Введено некорректное число");
+    a = int.Parse(Console.ReadLine()?? "0");
+}
+
+if (a >=1 && a < 6)
+{
+    Console.WriteLine("Будний день");
+}
+else
+{
+    Console.WriteLine("Выходной день");
+}
