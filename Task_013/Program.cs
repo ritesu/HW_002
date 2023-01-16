38ff7f6364412dc3f89ab1555eb4787c0c99f970
ritesu <ritesu@mail.ru>
2023-01-16 16:53:47 +0900

    Task_013

---
+++ Task_013/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()?? "0");
+if (a < 100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+else if (a >= 100 && a < 1000)
+{
+    int b = a % 10;
+    Console.WriteLine("Третье число равно");
+    Console.WriteLine(b);
+}
+else if (a >= 1000 && a < 10000)
+{
+    int c = (a / 10) % 10;
+    Console.WriteLine("Третье число равно");
+    Console.WriteLine(c);
+}
+else if (a >= 10000 && a < 100000)
+{
+    int d = (a / 100) % 10;
+    Console.WriteLine("Третье число равно");
+    Console.WriteLine(d);
+}
