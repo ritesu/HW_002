f197751be736cc84031037df760a39b0e1852e9f
ritesu <ritesu@mail.ru>
2023-01-18 01:51:32 +0900

    Task_001

---
+++ Task_001/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите номер четверти");
+int a = int.Parse(Console.ReadLine()?? "0");
+while (a < 0 || a > 4)
+{
+    Console.WriteLine("Некорректный ввод");
+    a = int.Parse(Console.ReadLine());
+}
+if (a == 1)
+{
+    Console.WriteLine("Для первой четверти: x (0; +∞), y (0; +∞)");
+}
+else if (a == 2)
+{
+    Console.WriteLine("Для второй четверти: x (0; -∞), y (0; +∞)");
+}
+else if (a == 3)
+{
+    Console.WriteLine("Для третьей четверти: x (0; -∞), y (0; -∞)");
+}
+else if (a == 4)
+{
+    Console.WriteLine("Для четвертой четверти: x (0; +∞), y (0; -∞)");
+}
